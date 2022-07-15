d5d9a30730ef40511642e9f3a918ca8181f0c15c
Ankush Singh <ankushsahil45@gmail.com>
2022-07-15 14:29:00 +0530

    minor change

---
+++ script/script.js
@@ -0,0 +1,77 @@
+function toggleHide() {
+    let btn = document.getElementById('btn1');
+    let description = document.getElementById('description1')
+    if (description.style.display != 'none') {
+        description.style.display = 'none';
+        btn1.innerHTML = '+';
+    }
+    else{
+        description.style.display = 'block';
+        btn1.innerHTML = 'x';
+    }
+}
+
+function toggleHide2() {
+    let btn = document.getElementById('btn2');
+    let description = document.getElementById('description2')
+    if (description.style.display != 'none') {
+        description.style.display = 'none';
+        btn2.innerHTML = '+';
+    }
+    else{
+        description.style.display = 'block';
+        btn2.innerHTML = 'x';
+    }
+}
+
+function toggleHide3() {
+    let btn = document.getElementById('btn3');
+    let description = document.getElementById('description3')
+    if (description.style.display != 'none') {
+        description.style.display = 'none';
+        btn3.innerHTML = '+';
+    }
+    else{
+        description.style.display = 'block';
+        btn3.innerHTML = 'x';
+    }
+}
+
+function toggleHide4() {
+    let btn = document.getElementById('btn4');
+    let description = document.getElementById('description4')
+    if (description.style.display != 'none') {
+        description.style.display = 'none';
+        btn4.innerHTML = '+';
+    }
+    else{
+        description.style.display = 'block';
+        btn4.innerHTML = 'x';
+    }
+}
+
+function toggleHide5() {
+    let btn = document.getElementById('btn5');
+    let description = document.getElementById('description5')
+    if (description.style.display != 'none') {
+        description.style.display = 'none';
+        btn5.innerHTML = '+';
+    }
+    else{
+        description.style.display = 'block';
+        btn5.innerHTML = 'x';
+    }
+}
+
+function toggleHide6() {
+    let btn = document.getElementById('btn6');
+    let description = document.getElementById('description6')
+    if (description.style.display != 'none') {
+        description.style.display = 'none';
+        btn6.innerHTML = '+';
+    }
+    else{
+        description.style.display = 'block';
+        btn6.innerHTML = 'x';
+    }
+}
